e032609bbe903e965fb800090e5f3b465c0cf1ff
Ma Jiaqi <72115906+majiaq@users.noreply.github.com>
2020-10-22 09:06:07 +0800

    Update MapManager_Reverse.cpp

--- MapManager/MapManager_Reverse.cpp
+++ MapManager/MapManager_Reverse.cpp
@@ -4,5 +4,10 @@
 void MapManager_Reverse(int player, int x, int y)
 {
   bool dir[8][2]={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-  
+  for(int i=0;i<8;i++)
+  {
+    int Reverse_Step=1;
+    int Reverse_Position_x=x+Reverse_Step_x*dir[i][0], Reverse_Position_y=y+Reverse_Step_y*dir[i][1];
+    while()
+  }
 }
