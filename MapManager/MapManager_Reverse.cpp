50c2d9dd8ff7828a9656a10bc6b257cf0348c0f9
Ma Jiaqi <72115906+majiaq@users.noreply.github.com>
2020-10-22 09:19:33 +0800

    Update MapManager_Reverse.cpp

--- MapManager/MapManager_Reverse.cpp
+++ MapManager/MapManager_Reverse.cpp
@@ -1,13 +1,31 @@
 #include "MapManager.h"
 
-//落子翻转
+bool dir[8][2]={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+//判断在棋盘内
+bool In_Range(int x,int y)
+{
+  return (1<=x && x<=8 && 1<=y && y<=8);
+}
+
+//翻转操作
+void Reverse(int x,int y,int xe,int ye,int d)
+{
+  int Reverse_x=x+dir[i][0], Reverse_y=y+dir[i][1];
+  while(Reverse_x!=xe && Reverse_y!=ye)
+  {
+    MapManager_Map[Reverse_x][Reverse_y]^=1;
+    Reverse_x+=dir[i][0];
+    Reverse_y+=dir[i][1];
+  }
+}
+
+//落子翻转调用接口
 void MapManager_Reverse(int player, int x, int y)
 {
-  bool dir[8][2]={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
   for(int i=0;i<8;i++)
   {
-    int Reverse_Step=1;
-    int Reverse_x=x+Reverse_Step*dir[i][0], Reverse_y=y+Reverse_Step*dir[i][1];
+    int Reverse_x=x+dir[i][0], Reverse_y=y+dir[i][1];
     while(In_Range(Reverse_x,Reverse_y))
     {
       if(MapManager_Map[Reverse_x][Reverse_y]==BLANK) break;
